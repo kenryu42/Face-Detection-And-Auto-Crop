9646cc5ff589eb0b5e73cae6aaa2223819d2f2bd
kenxdrgn <jliew@student.42tokyo.jp>
2021-02-12 00:38:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Face Detection And Crop
+
+---
+
+A python program that detects and crops faces in the image.
+
+# Installation
+
+---
+
+```bash
+pip install -r requirements.txt
+```
+
+# Usage
+
+---
+
+```bash
+python3 crop_face.py <image_path> ...
+```
---
+++ crop_face.py
@@ -0,0 +1,47 @@
+import cv2
+import sys
+import dlib
+from imutils import face_utils
+
+def crop_boundary(top, bottom, left, right, faces):
+    if faces:
+        top = max(0, top - 200)
+        left = max(0, left - 100)
+        right += 100
+        bottom += 100
+    else:
+        top = max(0, top - 50)
+        left = max(0, left - 50)
+        right += 50
+        bottom += 50
+
+    return (top, bottom, left, right)
+
+def crop_face(imgpath):
+    frame = cv2.imread(imgpath)
+    if frame is None:
+        return print(f"Invalid file path: [{imgpath}]")
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    face_detect = dlib.get_frontal_face_detector()
+    rects = face_detect(gray, 1)
+    if len(rects) is None:
+        return print("Sorry. HOG could not detect any faces from your image.")
+    for (i, rect) in enumerate(rects):
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        
+        top, bottom, left, right = crop_boundary(y, y + h, x, x + w, len(rects) <= 2)
+        crop_img_path = f"{imgpath.split('.')[0]}_crop_{i}.png"
+        crop_img = frame[top: bottom, left: right]
+        cv2.imwrite(crop_img_path, cv2.cvtColor(crop_img, cv2.COLOR_RGB2BGR))
+
+    return print(f"SUCCESS: [{imgpath}]")
+        
+def main(argv):
+    if len(argv) == 1:
+        sys.exit("Usage: python crop_face.py <image path> ...")
+    for imgpath in argv[1:]:
+        crop_face(imgpath)
+        
+if __name__ == "__main__":
+    main(sys.argv)
